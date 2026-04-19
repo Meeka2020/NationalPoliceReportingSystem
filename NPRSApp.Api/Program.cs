9200d3c6a76a606ab9a5f4b3aff763fbfb63057e
Meeka2020 <shamekameeka16@gmail.com>
2026-04-19 00:22:54 -0500

    Add ASP.NET Core Web API.

---
+++ NPRSApp.Api/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
